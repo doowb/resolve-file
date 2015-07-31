693ba53bb1e40dc362a717b416412e90066461a4
Brian Woodward <brian.woodward@gmail.com>
2015-07-31 19:58:02 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * resolve-file <https://github.com/doowb/resolve-file>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
